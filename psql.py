c6bec56665bb8ee7ae809069b9707bc60e97c93a
TaichiAkimoto <italyitalienitalia@gmail.com>
2017-07-11 15:48:25 +0900

    Implement first task

--- psql.py
+++ psql.py
@@ -6,30 +6,12 @@ dbname = "news"
 def popular_three_articles():
     conn = psycopg2.connect("dbname="+dbname)
     cur = conn.cursor()
-    cur.execute("select path, count(*) as num from log group by path order by num desc limit 4")
+    cur.execute("select articles.title, count(*) as num from articles join log on log.path like concat('%', articles.slug) group by articles.title order by num desc limit 3")
     articles = cur.fetchall()
-    result = ""
-    for i in range(1, 4):
-        slug = articles[i][0].split('/')[2]
-        #conn_2 = psycopg2.connect("dbname="+dbname)
-        #cur_2 = conn_2.cursor()
-        #query = "select title from articles where slug = {}".format(slug)
-        #cur_2.execute(query)
-        ## TODO cur_2.fetchall() inserted instead of slug ##
-        article = "\"" + slug + "\""
-        views = articles[i][1]
-        result = result + "{} - {} views\n".format(article, views)
-        #conn_2.close()
     conn.close()
-
+    result = ''
+    for i in range(0, 3):
+        result = result + "\"" + articles[i][0] + "\" - " + str(articles[i][1]) + " views\n"
     print(result)
 
 popular_three_articles()
-
-# 1. What are the most popular three articles of all time? Which articles have been accessed the most?
-# Present this information as a sorted list with the most popular article at the top.
-# reason code didn't move is that I didn't insert the result to variable.
-# How stupid I am.
-# "Princess Shellfish Marries Prince Handsome"1201 views
-# "Baltimore Ravens Defeat Rhode Island Shoggoths"915 views
-# "Political Scandal Ends In Political Scandal"553 views
