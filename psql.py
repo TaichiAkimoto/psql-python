4a2c39abf3804e56ef69a964e15abcb63a6ed183
TaichiAkimoto <italyitalienitalia@gmail.com>
2017-07-11 16:03:16 +0900

    Implement second task

--- psql.py
+++ psql.py
@@ -12,6 +12,20 @@ def popular_three_articles():
     result = ''
     for i in range(0, 3):
         result = result + "\"" + articles[i][0] + "\" - " + str(articles[i][1]) + " views\n"
+    print("*** What are the most popular three articles of all time?  ***")
+    print(result)
+
+def popular_authors():
+    conn = psycopg2.connect("dbname="+dbname)
+    cur = conn.cursor()
+    cur.execute("select authors.name, count(*) as num from authors join articles on authors.id = articles.author join log on log.path like concat('%', articles.slug) group by authors.name order by num desc")
+    authors = cur.fetchall()
+    conn.close()
+    result = ''
+    for i in range(0, len(authors)):
+        result = result + authors[i][0] + " - " + str(authors[i][1]) + " views\n"
+    print("*** Who are the most popular article authors of all time?   ***")
     print(result)
 
 popular_three_articles()
+popular_authors()
